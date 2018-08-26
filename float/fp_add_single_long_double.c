3c865e24999b0f511163abd32549c32edf6369f5
Acciente <221thunder@gmail.com>
2018-08-26 17:00:05 +0800

    Add long double operation.

---
+++ float/fp_add_single_long_double.c
@@ -0,0 +1,4 @@
+long double fp_add_single_long_double(long double x, long double y)
+{
+    return x + y;
+}
---
+++ float/fp_convert_single_long_double.c
@@ -0,0 +1,4 @@
+long double fp_convert_single_long_double(float x)
+{
+    return (long double)x;
+}
